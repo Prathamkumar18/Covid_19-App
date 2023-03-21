54c8e77a45a99420d28666d266657e90efa768fd
Pratham Kumar <pratham854305@gmail.com>
2023-03-21 11:34:28 +0530

    Animated splash Screen

---
+++ lib/Pages/SplashScreen.dart
@@ -0,0 +1,78 @@
+import 'dart:async';
+
+import 'package:covid_19/Pages/HomePage.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller =
+      AnimationController(duration: const Duration(seconds: 2), vsync: this)
+        ..repeat(reverse: true);
+
+  void initState() {
+    super.initState();
+    Timer(
+        const Duration(seconds: 5),
+        () => Navigator.push(
+            context, MaterialPageRoute(builder: (context) => HomePage())));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 4, 41, 61),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        // ignore: prefer_const_literals_to_create_immutables
+        children: [
+          Center(
+            child: AnimatedBuilder(
+              // ignore: sort_child_properties_last
+              child: Container(
+                height: 300,
+                width: 300,
+                child: Center(
+                    child: Image(
+                  image: AssetImage("Assets/virus.png"),
+                )),
+              ),
+              animation: _controller,
+              builder: (BuildContext context, Widget? child) {
+                return Transform.rotate(
+                  angle: _controller.value * 2.0 * 3.14159,
+                  child: child,
+                );
+              },
+            ),
+          ),
+          SizedBox(
+            height: 60,
+          ),
+          Center(
+            child: Text(
+              "Covid-19\nTracker App",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:covid_19/Pages/SplashScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
